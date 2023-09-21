260c8b70bec6e3fba6332363af7d9872588275fd
Onkar Vatsa <onkarvatsa@gmail.com>
2023-09-21 12:19:00 +0530

    ..

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -105,6 +105,8 @@ exports.paymentverification = async (req, res) => {
       }
 
       // Redirect to the success page
+      console.log(process.env.DOMAIN);
+
       res.redirect(
         `${process.env.DOMAIN}/success?razorpay_order_id=${razorpay_order_id}&razorpay_payment_id=${razorpay_payment_id}&razorpay_signature=${razorpay_signature}`
       );
